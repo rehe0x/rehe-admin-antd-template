6b9809af64934c9f4e800c6603d815c72b29f529
xiech <xiech@rehe.com>
2024-05-28 17:47:56 +0800

    登录页图片打包

--- src/page/login/Left.tsx
+++ src/page/login/Left.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Carousel } from 'antd';
+import a from "@/assets/img/1.jpg";
+import a1 from "@/assets/img/2.jpg";
+import a2 from "@/assets/img/3.png";
+import a3 from "@/assets/img/4.png";
+import a4 from "@/assets/img/6.jpg";
 
-const imgs = [
-  'src/assets/img/6.jpg',
-  'src/assets/img/2.jpg',
-  'src/assets/img/3.png',
-  'src/assets/img/4.png',
-  'src/assets/img/1.jpg',
-]
+
+// const imgs = [
+//   'src/assets/img/6.jpg',
+//   'src/assets/img/2.jpg',
+//   'src/assets/img/3.png',
+//   'src/assets/img/4.png',
+//   'src/assets/img/1.jpg',
+// ]
+
+const imgs = [a,a1,a2,a3,a4]
 
 let len = imgs.length, index; // index: 随机数
 for (let point = len - 1; point >= 0; point--) {
